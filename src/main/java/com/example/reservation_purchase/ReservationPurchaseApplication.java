5658960810c2c1e11d86dd6e16f984182d384877
jeongjihun <wlgns2234@naver.com>
2024-01-24 20:11:23 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/reservation_purchase/ReservationPurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservation_purchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationPurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationPurchaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reservation_purchase/ReservationPurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservation_purchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
