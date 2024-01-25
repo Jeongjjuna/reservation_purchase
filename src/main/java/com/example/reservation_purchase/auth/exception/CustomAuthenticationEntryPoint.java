0d1f995fd10d7f1cfb7574c5d54813583fe7c43e
jeongjihun <wlgns2234@naver.com>
2024-01-25 13:20:53 +0900

    feat: 인증 필터 구현
    - jwt를 사용하여 사용자 정보와 검증
    - 인증시 발생하는 예외를 처리할 수 있다.

---
+++ src/main/java/com/example/reservation_purchase/auth/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.example.reservation_purchase.auth.exception;
+
+import com.example.reservation_purchase.exception.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public CustomAuthenticationEntryPoint(final ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        log.error("[ERROR] occurs CustomAuthenticationEntryPoint -> commence()");
+        ErrorResponse body = new ErrorResponse(LocalDateTime.now(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        response.setContentType("application/json");
+        response.setStatus(401);
+        response.getWriter().write(objectMapper.writeValueAsString(body));
+    }
+}
--- src/main/java/com/example/reservation_purchase/config/SecurityConfig.java
+++ src/main/java/com/example/reservation_purchase/config/SecurityConfig.java
@@ -1,20 +1,39 @@
 package com.example.reservation_purchase.config;
 
+import com.example.reservation_purchase.auth.exception.CustomAuthenticationEntryPoint;
+import com.example.reservation_purchase.config.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    public SecurityConfig(final JwtAuthenticationFilter jwtAuthenticationFilter, final CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
+                        .requestMatchers(HttpMethod.POST, "/api/members").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(handle -> handle.authenticationEntryPoint(customAuthenticationEntryPoint))
                 .build();
     }
 
