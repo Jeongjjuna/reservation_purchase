bf9414d62a573e88c297475945b5df27b234e7c0
jeongjihun <wlgns2234@naver.com>
2024-01-29 19:09:39 +0900

    refactor: 테스트의 메서드 분리

--- src/test/java/com/example/reservation_purchase/member/domain/MemberCreateTest.java
+++ src/test/java/com/example/reservation_purchase/member/domain/MemberCreateTest.java
@@ -3,18 +3,46 @@ package com.example.reservation_purchase.member.domain;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("MemberCreate 단위테스트")
 class MemberCreateTest {
 
-    @DisplayName("비밀번호 8자리 미만이라면 예외발생")
+    private MemberCreate givenMemberCreate;
+
+    @BeforeEach
+    public void givenFollowerMember() {
+        givenMemberCreate = MemberCreate.builder()
+                .email("email_1@email.com")
+                .password("11111111")
+                .name("name")
+                .greetings("hello")
+                .authenticNumber("authenticationNumber")
+                .build();
+    }
+
+    @DisplayName("인증번호를 체크시 예외발생 테스트")
     @Test
-    void 비밀번호_길이_8자리_미만일때_예외발생() {
+    void 인증번호를_체크시_예외발생_테스트() {
+        assertThatThrownBy(() ->
+                givenMemberCreate.checkAuthenticated(null));
+        assertThatThrownBy(() ->
+                givenMemberCreate.checkAuthenticated("differentNumber"));
+        assertThatThrownBy(() ->
+                givenMemberCreate.checkAuthenticated(""));
+    }
+
+    @DisplayName("비밀번호 8자리 미만이라면 예외발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "1", "1234567"})
+    void 비밀번호_길이_8자리_미만일때_예외발생(String password) {
         MemberCreate memberCreate = MemberCreate.builder()
                 .email("test@email.com")
-                .password("1234567")
+                .password(password)
                 .name("name")
                 .greetings("hello")
                 .build();
