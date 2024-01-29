c91c5e3cc10536c5600d1c6a77e36f9efe31e5cb
jeongjihun <wlgns2234@naver.com>
2024-01-29 18:41:21 +0900

    refactor: 테스트의 메서드 분리

--- src/test/java/com/example/reservation_purchase/follow/domain/FollowTest.java
+++ src/test/java/com/example/reservation_purchase/follow/domain/FollowTest.java
@@ -3,38 +3,46 @@ package com.example.reservation_purchase.follow.domain;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.example.reservation_purchase.member.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 
+@DisplayName("Follow 단위테스트")
 class FollowTest {
+
+    private Member givenFollowerMember;
+
+    private Member givenFollowingMember;
+
+    @BeforeEach
+    public void givenFollowerMember() {
+        givenFollowerMember = Member.builder()
+                .id(1L)
+                .email("email_1@naver.com")
+                .password("11111111")
+                .name("name_1")
+                .greetings("hello_1")
+                .profileUrl("url_1")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        givenFollowingMember = Member.builder()
+                .id(1L)
+                .email("email_2@naver.com")
+                .password("22222222")
+                .name("name2")
+                .greetings("hello_2")
+                .profileUrl("url_2")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
 
     @DisplayName("생성 테스트")
     @Test
     void 생성테스트() {
-        Member followMember = Member.builder()
-                .id(1L)
-                .email("test1@naver.com")
-                .password("11111111")
-                .name("name1")
-                .greetings("hi1")
-                .profileUrl("url1")
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        Member followingMember = Member.builder()
-                .id(2L)
-                .email("test2@naver.com")
-                .password("22222222")
-                .name("name2")
-                .greetings("hi2")
-                .profileUrl("url2")
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        assertThatCode(() -> Follow.create(followMember, followingMember)
+        assertThatCode(() -> Follow.create(givenFollowerMember, givenFollowingMember)
         ).doesNotThrowAnyException();
     }
 
