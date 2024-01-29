d486a25c860957df3cedb42d0bfc1e1534757487
jeongjihun <wlgns2234@naver.com>
2024-01-29 19:16:26 +0900

    refactor: 파라미터 테스트 이름 추가

--- src/test/java/com/example/reservation_purchase/member/domain/MemberCreateTest.java
+++ src/test/java/com/example/reservation_purchase/member/domain/MemberCreateTest.java
@@ -37,7 +37,7 @@ class MemberCreateTest {
     }
 
     @DisplayName("비밀번호 8자리 미만이라면 예외발생")
-    @ParameterizedTest
+    @ParameterizedTest(name = "비밀번호가 \"{0}\" 일 때")
     @ValueSource(strings = {"", "1", "1234567"})
     void 비밀번호_길이_8자리_미만일때_예외발생(String password) {
         MemberCreate memberCreate = MemberCreate.builder()
