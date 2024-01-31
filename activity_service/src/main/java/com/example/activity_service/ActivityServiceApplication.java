c76bf06de137383dab40c45c41c2fbba1807763d
jeongjihun <wlgns2234@naver.com>
2024-01-31 13:51:28 +0900

    feat: 서브모듈 분리
    - user_service, newsfeed_service, activity_service

---
+++ activity_service/src/main/java/com/example/activity_service/ActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.activity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityServiceApplication.class, args);
+    }
+
+}
---
+++ newsfeed_service/src/main/java/com/example/newsfeed_service/NewsfeedServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.newsfeed_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedServiceApplication {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        SpringApplication.run(NewsfeedServiceApplication.class, args);
+    }
+
+}
