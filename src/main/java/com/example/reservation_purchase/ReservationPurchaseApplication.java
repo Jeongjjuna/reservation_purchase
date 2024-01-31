8ea2283c9cff0ab9aea0b88ffc382efcf2b020c9
jeongjihun <wlgns2234@naver.com>
2024-01-31 17:57:42 +0900

    feat: 3개의 서비스로 분리
    - user_service(8080)
    - activity_service(8081)
    - newsfeed_service(8082)

--- src/main/java/com/example/reservation_purchase/ReservationPurchaseApplication.java
+++ src/main/java/com/example/reservation_purchase/ReservationPurchaseApplication.java
@@ -1,11 +1,9 @@
 package com.example.reservation_purchase;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
-@EnableJpaAuditing
+//@SpringBootApplication
+//@EnableJpaAuditing
 public class ReservationPurchaseApplication {
 
     public static void main(String[] args) {
