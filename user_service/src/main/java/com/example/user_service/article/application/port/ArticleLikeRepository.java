32aad319eb51c448884d5ed20f538da8667d7c68
jeongjihun <wlgns2234@naver.com>
2024-02-01 02:11:42 +0900

    feat: user_service 로직만 남기고 지우기

--- user_service/src/main/java/com/example/user_service/article/application/port/ArticleLikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.article.application.port;
-
-import com.example.user_service.article.domain.ArticleLike;
-
-public interface ArticleLikeRepository {
-    ArticleLike save(ArticleLike articleLike);
-}
--- user_service/src/main/java/com/example/user_service/article/application/port/CommentLikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.article.application.port;
-
-import com.example.user_service.article.domain.CommentLike;
-
-public interface CommentLikeRepository {
-    CommentLike save(CommentLike commentLike);
-}
