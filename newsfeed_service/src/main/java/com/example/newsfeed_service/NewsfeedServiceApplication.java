6636788e670877776c871e21f2b7323792132851
jeongjihun <wlgns2234@naver.com>
2024-01-31 14:47:17 +0900

    feat: 서브모듈 분리

--- newsfeed_service/src/main/java/com/example/newsfeed_service/NewsfeedServiceApplication.java
+++ newsfeed_service/src/main/java/com/example/newsfeed_service/NewsfeedServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewsfeedServiceApplication {
 
     public static void main(String[] args) {
-        System.out.println("test");
         SpringApplication.run(NewsfeedServiceApplication.class, args);
     }
 
