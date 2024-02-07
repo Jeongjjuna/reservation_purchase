595fe3b08bd94f0f7d5fff71bfca38b7825fb9f7
jeongjihun <wlgns2234@naver.com>
2024-02-07 12:35:42 +0900

    feat: ecommerce_service 모듈 추가

---
+++ module_ecommerce_service/src/main/java/com/example/module_ecommerce_service/EcommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.module_ecommerce_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceServiceApplication.class, args);
+    }
+
+}
