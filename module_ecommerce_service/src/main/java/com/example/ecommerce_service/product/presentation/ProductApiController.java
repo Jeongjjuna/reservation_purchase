ceaffca5da5995ecec26d940f01f9baaf226f521
jeongjihun <wlgns2234@naver.com>
2024-02-07 15:17:36 +0900

    feat: 상품 단건조회 기능 구현

--- module_ecommerce_service/src/main/java/com/example/ecommerce_service/product/presentation/ProductApiController.java
+++ module_ecommerce_service/src/main/java/com/example/ecommerce_service/product/presentation/ProductApiController.java
@@ -2,8 +2,12 @@ package com.example.ecommerce_service.product.presentation;
 
 import com.example.ecommerce_service.common.response.Response;
 import com.example.ecommerce_service.product.application.ProductService;
+import com.example.ecommerce_service.product.domain.Product;
 import com.example.ecommerce_service.product.domain.ProductCreate;
+import com.example.ecommerce_service.product.presentation.response.ProductResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +24,14 @@ public class ProductApiController {
     public Response<Void> create(@RequestBody final ProductCreate productCreate) {
         productService.create(productCreate);
         return Response.success();
+    }
+
+    /**
+     * 상품 단건 조회
+     */
+    @GetMapping("/{id}")
+    public Response<ProductResponse> read(@PathVariable Long id) {
+        Product product = productService.find(id);
+        return Response.success(ProductResponse.from(product));
     }
 }
--- module_ecommerce_service/src/test/java/com/example/ecommerce_service/product/presentation/ProductApiControllerTest.java
+++ module_ecommerce_service/src/test/java/com/example/ecommerce_service/product/presentation/ProductApiControllerTest.java
@@ -1,8 +1,12 @@
 package com.example.ecommerce_service.product.presentation;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.ecommerce_service.product.application.port.ProductRepository;
+import com.example.ecommerce_service.product.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,30 @@ class ProductApiControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DisplayName("상품 단건조회 테스트 : 성공")
+    @Test
+    void 상품_단건_조회_요청() throws Exception {
+        // given
+        Product product = Product.builder()
+                .name("name")
+                .content("content")
+                .price(20000L)
+                .build();
+        Product saved = productRepository.save(product);
+
+        // when, then
+        mockMvc.perform(get("/v1/products/{productId}", saved.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.desc").value("success"))
+                .andExpect(jsonPath("$.data.name").value("name"))
+                .andExpect(jsonPath("$.data.content").value("content"))
+                .andExpect(jsonPath("$.data.price").value("20000"));
+    }
+
     @DisplayName("상품 생성 테스트 : 성공")
     @Test
     void 상품_생성_요청() throws Exception {
@@ -37,7 +65,8 @@ class ProductApiControllerTest {
         mockMvc.perform(post("/v1/products")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.desc").value("success"));
     }
 
 }
