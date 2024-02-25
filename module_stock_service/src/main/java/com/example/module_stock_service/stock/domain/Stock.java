f86a17e3c22007e6e654123a382de4f33bb1f1ca
jeongjihun <wlgns2234@naver.com>
2024-02-26 02:11:58 +0900

    test: 재고 단위 테스트 추가
    - 단순하게 메서드 이름 변경(add, subtract)
    - 재고감소, 재고증가, 유효성검사 테스트 추가

--- module_stock_service/src/main/java/com/example/module_stock_service/stock/domain/Stock.java
+++ module_stock_service/src/main/java/com/example/module_stock_service/stock/domain/Stock.java
@@ -30,18 +30,21 @@ public class Stock {
     }
 
     public Stock update(final Integer stockCount) {
+        if (stockCount  < 0) {
+            throw new GlobalException(HttpStatus.CONFLICT, "재고 개수가 0보다 작습니다.");
+        }
         this.stockCount = stockCount;
         return this;
     }
 
     public Stock validateStock() {
         if (stockCount  <= 0) {
-            throw new IllegalArgumentException("재고 개수가 0보다 같거나 작습니다.");
+            throw new GlobalException(HttpStatus.CONFLICT, "재고 개수가 0보다 같거나 작습니다.");
         }
         return this;
     }
 
-    public Stock subtractStock(int quantity) {
+    public Stock subtract(int quantity) {
         System.out.println(stockCount + " : " + quantity);
         if (stockCount < quantity) {
             throw new GlobalException(HttpStatus.CONFLICT, "재고수량이 부족합니다.");
@@ -50,7 +53,7 @@ public class Stock {
         return this;
     }
 
-    public Stock addStock(int quantity) {
+    public Stock add(int quantity) {
         stockCount = stockCount + quantity;
         return this;
     }
--- module_stock_service/src/test/java/com/example/module_stock_service/stock/presentation/RedisTest.java
+++ module_stock_service/src/test/java/com/example/module_stock_service/stock/presentation/RedisTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@DisplayName("redis 동작 확인용 테스트")
 @SpringBootTest
 public class RedisTest {
 
